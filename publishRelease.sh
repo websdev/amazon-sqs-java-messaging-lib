a15ada455c2436f33eb2a1054c00d3f3bcbacf56
shwetav94 <shwetav94@gmail.com>
2019-01-10 15:36:47 -0500

    DG-22117: published

--- publishRelease.sh
+++ publishRelease.sh
@@ -4,7 +4,7 @@ RELEASE_VERSION="$(grep -F "<version>" pom.xml | head -n 1 | sed 's/<[^>]*>//g' 
 REPOSITORY_VERSION_URL="http://nexus.digital.vistaprint.io/repository/public/com/webs/amazon-sqs-java-messaging-lib/$RELEASE_VERSION/amazon-sqs-java-messaging-lib-$RELEASE_VERSION.pom"
 curl -s --head "$REPOSITORY_VERSION_URL" | grep -F "404 Not Found"
 if [ "$?" -eq 0 ]; then
-    mvn deploy
+    mvn deploy -DskipTests -e --global-settings ./settings.xml
     exit $?
 else
     echo 'Version not published because it was already published'
